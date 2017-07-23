238daf0d8b2f9f779476860e5a4dde601f43007b
Damien Cassu <damiencassu@users.noreply.github.com>
2017-07-23 17:53:55 +0200

    Polynectar V 0.0.2

--- js/background.js
+++ js/background.js
@@ -1,17 +1,7 @@
-//Registered user-agents
-var userAgents = new Array();
-userAgents["Linux_Chrome"]="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
-userAgents["Windows_Chrome"]="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
-userAgents["MacOS_Chrome"]="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
-userAgents["Linux_Firefox"]="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0";
-userAgents["Windows_Firefox"]="Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0";
-userAgents["MacOS_Firefox"]="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:51.0) Gecko/20100101 Firefox/51.0";
-userAgents["Linux_Opera"]="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991";
-userAgents["Windows_Opera"]="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991";
-userAgents["MacOS_Opera"]="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.18.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991"; 
-
 //Function in charge of user-agent forgery before HTTP Request sending
 var updateUA = function(details) {
+
+	var userAgents = JSON.parse(localStorage["userAgents"]);
 
 	if(localStorage["state"] === "ON"){
 		for (var i = 0; i < details.requestHeaders.length; ++i) {
@@ -21,6 +11,7 @@ var updateUA = function(details) {
 			}
 		}
 	}
+
 	return {requestHeaders: details.requestHeaders};
 };
 
@@ -43,7 +34,16 @@ var init = function(details){
 
 	if(!localStorage["os"]){
 		localStorage["os"] = "Windows";
-	}	
+	}
+
+	//User-agents' list loading
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", chrome.extension.getURL("data/user_agents.json"), false);
+	xhr.send(null);	
+
+	if(xhr.status === 200){
+		localStorage["userAgents"] = xhr.responseText;
+	}
 } 
 
 //updateUA function launched after each HTTP header creation by the browser
